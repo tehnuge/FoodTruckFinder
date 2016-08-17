f4e37e894dd8e66ec291efcd6098d4d533d0f17b
tehnuge <jhnduong@gmail.com>
2016-08-17 10:42:51 -0700

    added styling

--- bin/bundle.js
+++ bin/bundle.js
@@ -46,8 +46,8 @@
 
 	'use strict';
 	
-	var $ = __webpack_require__(2),
-	    App = __webpack_require__(1),
+	var $ = __webpack_require__(1),
+	    App = __webpack_require__(2),
 	    url = 'https://maps.googleapis.com/maps/api/js',
 	    key = 'AIzaSyD5vQG68S8OQc2Fdwz2oIDpdE91gd96Ua0',
 	    fullUrl = url + '?key=' + key;
@@ -58,131 +58,6 @@
 
 /***/ },
 /* 1 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	var $ = __webpack_require__(2);
-	var utils = __webpack_require__(3);
-	var _exports = module.exports = {};
-	
-	var SFGovUrl = 'https://data.sfgov.org/resource/6a9r-agq8.json',
-	    SFGovKey = 'RzO65y75nj0P8erDPoh90p2Go',
-	    gMapsUrl = 'https://maps.googleapis.com/maps/api/geocode/json',
-	    gMapsKey = 'AIzaSyD5vQG68S8OQc2Fdwz2oIDpdE91gd96Ua0',
-	    lat,
-	    lng,
-	    map,
-	    infowindow,
-	    range,
-	    markers = [],
-	    center = { lat: 37.756367, lng: -122.44370 },
-	    NanDiv = document.getElementById("Nan"),
-	    noResultsDiv = document.getElementById("noResults");
-	
-	// Google Maps initialization
-	_exports.initMap = function () {
-		map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 12,
-			center: center
-		});
-	};
-	
-	// Add event handlers for input fields
-	$("#userInputs").keypress(function (event) {
-		var inputAddress = $('#address').val(),
-		    inputRange = $('#range').val();
-		if (event.which == 13) {
-			validateInputs(inputAddress, inputRange);
-		}
-	});
-	
-	$("#submit").click(function () {
-		var inputAddress = $('#address').val(),
-		    inputRange = $('#range').val();
-		validateInputs(inputAddress, inputRange);
-	});
-	
-	var validateInputs = function validateInputs(inputAddress, inputRange) {
-		noResultsDiv.style.display = 'none';
-		NanDiv.style.display = 'none';
-		if (inputRange !== null && utils.isNumeric(inputRange)) {
-			range = inputRange * 1609;
-		} else {
-			NanDiv.style.display = 'block';
-			return 0;
-		}
-		return getLocation(inputAddress);
-	};
-	
-	// Get lat and lng coordinates of address from Google Maps API
-	var getLocation = function getLocation(address) {
-		$.ajax({
-			type: 'GET',
-			url: gMapsUrl,
-			data: {
-				key: gMapsKey,
-				address: address
-			},
-			success: function success(data) {
-				lat = data.results[0].geometry.location.lat;
-				lng = data.results[0].geometry.location.lng;
-	
-				// Clear markers and empty markers array if a query was made already
-				for (var i = 0; i < markers.length; i++) {
-					markers[i].setMap(null);
-				}
-				markers = [];
-	
-				return getTrucks(lat, lng);
-			}
-		});
-	};
-	
-	// Run address through SF OpenData API
-	var getTrucks = function getTrucks(lat, lng) {
-		$.ajax({
-			type: 'GET',
-			url: SFGovUrl,
-			data: {
-				$$app_token: SFGovKey,
-				$where: 'within_circle(location,' + lat + ', ' + lng + ', ' + range + ')'
-			},
-			success: function success(data) {
-				// Let user know if no results returned
-				if (data.length === 0) {
-					noResultsDiv.style.display = 'block';
-				}
-	
-				var _loop = function _loop(i) {
-					markers.push(new google.maps.Marker({
-						position: { lat: parseFloat(data[i].latitude), lng: parseFloat(data[i].longitude) },
-						map: map,
-						title: data[i].applicant
-					}));
-	
-					markers[i].addListener('click', function () {
-						var contentString = '<b>' + data[i].applicant + ':</b> <br /> Hours: ' + data[i].dayshours + '<br /> Details: ' + data[i].fooditems;
-						if (infowindow) {
-							infowindow.close();
-						}
-						infowindow = new google.maps.InfoWindow({
-							content: contentString
-						});
-						infowindow.open(map, markers[i]);
-					});
-				};
-	
-				for (var i = 0; i < data.length; i++) {
-					_loop(i);
-				}
-			}
-		});
-		return markers;
-	};
-
-/***/ },
-/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*eslint-disable no-unused-vars*/
@@ -10260,6 +10135,131 @@
 	return jQuery;
 	} );
 
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var $ = __webpack_require__(1);
+	var utils = __webpack_require__(3);
+	var _exports = module.exports = {};
+	
+	var SFGovUrl = 'https://data.sfgov.org/resource/6a9r-agq8.json',
+	    SFGovKey = 'RzO65y75nj0P8erDPoh90p2Go',
+	    gMapsUrl = 'https://maps.googleapis.com/maps/api/geocode/json',
+	    gMapsKey = 'AIzaSyD5vQG68S8OQc2Fdwz2oIDpdE91gd96Ua0',
+	    lat,
+	    lng,
+	    map,
+	    infowindow,
+	    range,
+	    markers = [],
+	    center = { lat: 37.756367, lng: -122.44370 },
+	    NanDiv = document.getElementById("Nan"),
+	    noResultsDiv = document.getElementById("noResults");
+	
+	// Google Maps initialization
+	_exports.initMap = function () {
+		map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 12,
+			center: center
+		});
+	};
+	
+	// Add event handlers for input fields
+	$("#userInputs").keypress(function (event) {
+		var inputAddress = $('#address').val(),
+		    inputRange = $('#range').val();
+		if (event.which == 13) {
+			validateInputs(inputAddress, inputRange);
+		}
+	});
+	
+	$("#submit").click(function () {
+		var inputAddress = $('#address').val(),
+		    inputRange = $('#range').val();
+		validateInputs(inputAddress, inputRange);
+	});
+	
+	var validateInputs = function validateInputs(inputAddress, inputRange) {
+		noResultsDiv.style.display = 'none';
+		NanDiv.style.display = 'none';
+		if (inputRange !== null && utils.isNumeric(inputRange)) {
+			range = inputRange * 1609;
+		} else {
+			NanDiv.style.display = 'block';
+			return 0;
+		}
+		return getLocation(inputAddress);
+	};
+	
+	// Get lat and lng coordinates of address from Google Maps API
+	var getLocation = function getLocation(address) {
+		$.ajax({
+			type: 'GET',
+			url: gMapsUrl,
+			data: {
+				key: gMapsKey,
+				address: address
+			},
+			success: function success(data) {
+				lat = data.results[0].geometry.location.lat;
+				lng = data.results[0].geometry.location.lng;
+	
+				// Clear markers and empty markers array if a query was made already
+				for (var i = 0; i < markers.length; i++) {
+					markers[i].setMap(null);
+				}
+				markers = [];
+	
+				return getTrucks(lat, lng);
+			}
+		});
+	};
+	
+	// Run address through SF OpenData API
+	var getTrucks = function getTrucks(lat, lng) {
+		$.ajax({
+			type: 'GET',
+			url: SFGovUrl,
+			data: {
+				$$app_token: SFGovKey,
+				$where: 'within_circle(location,' + lat + ', ' + lng + ', ' + range + ')'
+			},
+			success: function success(data) {
+				// Let user know if no results returned
+				if (data.length === 0) {
+					noResultsDiv.style.display = 'block';
+				}
+	
+				var _loop = function _loop(i) {
+					markers.push(new google.maps.Marker({
+						position: { lat: parseFloat(data[i].latitude), lng: parseFloat(data[i].longitude) },
+						map: map,
+						title: data[i].applicant
+					}));
+	
+					markers[i].addListener('click', function () {
+						var contentString = '<b>' + data[i].applicant + ':</b> <br /> Hours: ' + data[i].dayshours + '<br /> Details: ' + data[i].fooditems;
+						if (infowindow) {
+							infowindow.close();
+						}
+						infowindow = new google.maps.InfoWindow({
+							content: contentString
+						});
+						infowindow.open(map, markers[i]);
+					});
+				};
+	
+				for (var i = 0; i < data.length; i++) {
+					_loop(i);
+				}
+			}
+		});
+		return markers;
+	};
 
 /***/ },
 /* 3 */
